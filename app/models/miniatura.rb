167161971cb1687a40bd28ee8da55724fd902937
Andrei <andreipaesfigueiredo@gmail.com>
2018-10-02 00:48:49 -0300

    quantidade_miniatura_reserva

--- app/models/miniatura.rb
+++ app/models/miniatura.rb
@@ -1,3 +1,8 @@
 class Miniatura < ApplicationRecord
     has_and_belongs_to_many :reservas
+
+    def getQuantidade(reserva)
+        @miniaturaReserva = MiniaturasReserva.where(:miniatura_id => self.id, :reserva_id => reserva.id).take
+        return @miniaturaReserva.quantidade 
+    end
 end
